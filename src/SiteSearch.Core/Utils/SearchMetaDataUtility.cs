dedbfdaf1e76e87b50c4584d0afa6a946fac2d2a
aeagle <allan@allaneagle.com>
2019-10-02 00:00:57 +0100

    Added simple metadata caching per type

--- src/SiteSearch.Core/Utils/SearchMetaDataUtility.cs
+++ src/SiteSearch.Core/Utils/SearchMetaDataUtility.cs
@@ -1,35 +1,46 @@
 ﻿using SiteSearch.Core.Models;
 using SiteSearch.Core.Extensions;
 using System.Reflection;
+using System.Collections.Concurrent;
+using System;
 
 namespace SiteSearch.Core.Utils
 {
     public static class SearchMetaDataUtility
     {
+        private static ConcurrentDictionary<Type, SearchMetaData> metaDataCache = 
+            new ConcurrentDictionary<Type, SearchMetaData>();
+
         public static SearchMetaData GetMetaData<T>() where T : class
         {
-            var result = new SearchMetaData();
-
-            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            foreach (var property in properties)
-            {
-                if (property.CanRead && !property.IsPropertyACollection())
+            return metaDataCache.GetOrAdd(
+                typeof(T),
+                (type) =>
                 {
-                    result.Fields.Add(
-                        property.Name.ToLower(),
-                        new SearchFieldInfo
-                        {
-                            PropertyInfo = property,
-                            Id = property.HasAttribute<IdAttribute>(),
-                            Keyword = property.HasAttribute<KeywordAttribute>(),
-                            Store = property.HasAttribute<StoreAttribute>(),
-                            Facet = property.HasAttribute<TermFacetAttribute>()
-                        }
-                    );
-                }
-            }
+                    var result = new SearchMetaData();
 
-            return result;
+                    var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                    foreach (var property in properties)
+                    {
+                        if (property.CanRead && !property.IsPropertyACollection())
+                        {
+                            result.Fields.Add(
+                                property.Name.ToLower(),
+                                new SearchFieldInfo
+                                {
+                                    PropertyInfo = property,
+                                    Id = property.HasAttribute<IdAttribute>(),
+                                    Keyword = property.HasAttribute<KeywordAttribute>(),
+                                    Store = property.HasAttribute<StoreAttribute>(),
+                                    Facet = property.HasAttribute<TermFacetAttribute>()
+                                }
+                            );
+                        }
+                    }
+
+                    return result;
+                }
+            );
         }
     }
 }
